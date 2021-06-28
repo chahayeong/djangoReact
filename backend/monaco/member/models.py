accd1cf69405c6da9cf0d948e56f71db8d8fab26
chahayeong <c6prince@naver.com>
2021-06-28 18:04:32 +0900

    gaip

---
+++ backend/monaco/member/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Member(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    created_at = models.DateTimeField()
+
+    class Meta:
+        managed = True
+        db_table = 'members'
+
+# Create your models here.
---
+++ frontend/src/common/index.js
@@ -0,0 +1,2 @@
+export { default as Nav } from './Nav'
+export { UserMenu, BlogMenu, ItemMenu, StockMenu } from './Menu'
